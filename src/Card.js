eb570e635a427f9430a8af7aab44c2507bdb58ef
Ganesh Nomula <gnomula198@gmail.com>
2019-03-05 22:37:26 -0600

    Linking local and remote

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+	return(
+		<div>
+			<img alt='robots' src = ''/>
+			<div>
+				<h2>Smith Grame</h2>
+				<p>smithg@gmail.com</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
