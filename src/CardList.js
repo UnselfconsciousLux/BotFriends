f5f665fc0f4691fce0e590b7c5e241377805f643
Ganesh Nomula <gnomula198@gmail.com>
2019-03-07 11:01:32 -0600

    Search Implemented Successfully

--- src/CardList.js
+++ src/CardList.js
@@ -7,7 +7,7 @@ const CardList = ({robots}) => {
 	const CardComponent = robots.map((user, i) => {
 		return (
 				<Card 
-				key={robots[i].id} 
+				key={i} 
 				id={robots[i].id} 
 				name={robots[i].name} 
 				email={robots[i].email}
