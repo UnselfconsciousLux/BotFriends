92c795c2ebe822b1d3e7c00e8eebc515161396e9
Ganesh Nomula <gnomula198@gmail.com>
2019-03-10 18:06:07 -0500

    commit 10/3

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import Searchbox from './Searchbox';
-import {robots} from './robots';
+//import {robots} from './robots';
 import './App.css';
 
 // STATE - Describes the STATE of the application
 // PROPS - are simply things comes out of state
 
 class App extends Component {
-
+//smart components tend to have a class
 	constructor() {
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
 	}
 
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => {this.setState({robots: users})});
+	}
+
 	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value})
+		this.setState({ searchfield: event.target.value});
 	}
 
 	render(){
 		const filteredRobots = this.state.robots.filter(robot => {
 		return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		return(
-			<div className='tc'>
-				<h1 className = 'f1'>bot friends</h1>
-				<Searchbox searchChange = {this.onSearchChange}/>	
-				<CardList robots={filteredRobots}/>
-			</div>
-		);
+
+		if(this.state.robots.length === 0){
+			return <h1>loading...</h1>
+		}
+		else {
+			return(
+				<div className='tc'>
+					<h1 className = 'f1'>bot friends</h1>
+					<Searchbox searchChange = {this.onSearchChange}/>	
+					<CardList robots={filteredRobots}/>
+				</div>
+			);
+
+		}
+
 	}
 }
 
